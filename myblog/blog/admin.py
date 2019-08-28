422672626b58c40fc09d17824d240b2753422abc
wo16feng <328515384@qq.com>
2019-08-28 20:14:03 +0800

    del myblog

--- myblog/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Article
-# Register your models here.
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content','time')
-    list_filter = ('time',)
-
-admin.site.register(Article,ArticleAdmin)
-# admin.register(Article)
