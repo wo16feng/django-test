21e7ee9d8435ffb2e435fdb603d44a63dbeeeea3
wo16feng <328515384@qq.com>
2019-09-23 14:23:01 +0800

    del testtool

--- testtool/testweb/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AddressInfo
-# Register your models here.
-class AddressInfoAdmin(admin.ModelAdmin):
-    list_display = ['address','pid']
-admin.site.register(AddressInfo,AddressInfoAdmin)
--- testtool/testweb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestwebConfig(AppConfig):
-    name = 'testweb'
-
